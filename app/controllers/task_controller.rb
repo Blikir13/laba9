4964a1630d04f8995e324331c143a97ca40e9902
Blikir13 <Kirilltatarenko03@yandex.ru>
2022-11-18 22:37:52 +0300

    lab9 without tests

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -10,10 +10,6 @@ class TaskController < ApplicationController
     mas = @number.map(&:to_i)
     @array = edit(mas)
     @s = max(@array)
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 
   private
@@ -42,16 +38,11 @@ class TaskController < ApplicationController
 
   # описание возможных ошибок
   def error(str)
-    if str.split(' ').size < 10
-      flash[:error] = 'Вы ввели меньше 10 чисел'
-      redirect_to root_path
-      return
-    end
     str.split(' ').each do |x|
       next unless x.match(/^-?[0-9]\d*$/).nil?
 
-      flash[:error] = 'Вы ввели не число / не целое число!'
-      redirect_to root_path
+      flash[:notice] = 'Вы ввели не число'
     end
+    flash[:notice] = 'Вы ввели меньше 10 чисел' if str.split(' ').size < 10
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   root 'task#input'
+  get 'task/input', to: 'task#input'
   post 'task/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
