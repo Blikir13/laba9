d1803bbe98d634c62593bf7cc10b3b1b048a8601
Blikir13 <Kirilltatarenko03@yandex.ru>
2022-12-02 21:20:14 +0300

    correct

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Static content', type: :system do
-  # автоматически создаем значения x и y
+  # автоматически создаем значениe number
   let(:x_value) { '9 8 7 6 5 4 3 2 1 0' }
 
-  # сценарий успешного складывания x + y
   scenario 'check' do
     visit root_path # переходим на страницы ввода
-    fill_in :number, with: x_value # заполняем поле с name="x"
+    fill_in :number, with: x_value # заполняем поле с name="number"
     find('#calculate-btn').click # нажимаем на кнопку с id="calculate_btn"
     # ожидаем найти в контенере вывода правильное содержимое
     expect(find('#resTable')).to have_text("введенная последовательность 9 8 7 6 5 4 3 2 1 0\nМаксимальная подпоследовательность Нет максимальной")
   end
-  scenario 'check' do
+  scenario 'new_check' do
     visit root_path # переходим на страницы ввода
-    fill_in :number, with: '1 2' # заполняем поле с name="x"
+    fill_in :number, with: '1 2' # заполняем поле с name="number"
     find('#calculate-btn').click # нажимаем на кнопку с id="calculate_btn"
     # ожидаем найти в контенере вывода правильное содержимое
     expect(find('#notice')).to have_text('Вы ввели меньше 10 чисел')
